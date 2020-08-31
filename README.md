932c63da803a082b2b3623a127674ae24a94ed84
Wesley Luyten <me@wesleyluyten.com>
2020-08-30 21:59:54 -0400

    Add alpha version

---
+++ README.md
@@ -0,0 +1,15 @@
+# eleventy-plugin-sharp
+
+**npm**: `npm i eleventy-plugin-sharp`  
+
+Inspired by the [Craft CMS image transform API](https://craftcms.com/docs/3.x/image-transforms.html).  
+This plugin gives you the full power of the [sharp](https://sharp.pixelplumbing.com/) library in your [11ty](https://www.11ty.dev/) templates.
+
+```njk
+{% set image = "/images/zen-pond.jpg" | sharp %}
+
+<picture>
+    <source srcset="{% getUrl image | resize({ width: 1440, height: 460 }) %} 1x, {% getUrl image | resize({ width: 2560, height: 800 }) %} 2x" media="(min-width: 640px)">
+    <img class="articleHero-image" srcset="{% getUrl image | resize({ width: 640, height: 320 }) %} 1x, {% getUrl image | resize({ width: 1280, height: 640 }) %} 2x" alt="{{ image.title }}">
+</picture>
+```
---
+++ eleventy-plugin-sharp.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const sharp = require('sharp');
+const debug = require('debug')('EleventyPluginSharp');
+
+const exclude = [
+  'clone',
+  'metadata',
+  'stats',
+  'trim', // trim is a built-in filter for most templating languages
+  'toFile',
+  'toBuffer',
+];
+
+const forwardMethods = Object.keys(sharp.prototype).filter((method) => {
+  return !method.startsWith('_') && !exclude.includes(method);
+});
+
+const globalOptions = {
+  outputDir: 'public/images',
+};
+
+function createSharpPlugin(options) {
+  options = Object.assign({}, globalOptions, options);
+
+  return function sharpPlugin(eleventyConfig) {
+    /**
+     * Sharp filters
+     */
+    eleventyConfig.addFilter('sharp', sharp);
+
+    forwardMethods.forEach((method) => {
+      eleventyConfig.addFilter(method, function (instance, ...args) {
+        if (typeof instance === 'string') instance = sharp(instance);
+        return instance[method](...args);
+      });
+    });
+
+    eleventyConfig.addFilter('toFile', function (instance, fileOut) {
+      if (typeof instance === 'string') instance = sharp(instance);
+      instance.fileOut = fileOut;
+      return instance;
+    });
+
+    /**
+     * Async shortcodes
+     */
+    eleventyConfig.addAsyncShortcode('getUrl', async function (instance) {
+      if (!instance.fileOut) {
+        instance.fileOut = path.join(
+          options.outputDir,
+          createFileOutName(instance.options)
+        );
+      }
+
+      debug('Writing %o', instance.fileOut);
+      await instance.toFile(instance.fileOut);
+      return instance.fileOut;
+    });
+
+    eleventyConfig.addAsyncShortcode('getWidth', async function (instance) {
+      const metadata = await getMetadata(instance.fileOut);
+      return `${metadata.width}`;
+    });
+
+    eleventyConfig.addAsyncShortcode('getHeight', async function (instance) {
+      const metadata = await getMetadata(instance.fileOut);
+      return `${metadata.height}`;
+    });
+
+    function getMetadata(instance) {
+      if (typeof instance === 'string') instance = sharp(instance);
+      return instance.metadata();
+    }
+  };
+}
+
+/**
+ * Adds a postfix to the file out name.
+ * @param  {Object} options
+ * @return {String}
+ */
+function createFileOutName(opts) {
+  const { width, height, formatOut } = opts;
+
+  let postfix = '';
+  if (width > 0 || height > 0) {
+    if (width) {
+      postfix += width;
+      if (height) postfix += 'x';
+    }
+    if (height) {
+      postfix += height;
+      if (!width) postfix += 'h';
+    }
+  }
+  if (postfix) postfix = `-${postfix}`;
+
+  const input = opts.input.file;
+  const extname = formatOut === 'input' ? path.extname(input) : `.${formatOut}`;
+  const basename = path.basename(input, path.extname(input));
+
+  return `${basename}${postfix}${extname}`;
+}
+
+module.exports = createSharpPlugin;
